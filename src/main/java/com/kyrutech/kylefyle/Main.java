bb8a69c10dc87779978edf814b80c638711865b8
Kyle Rudy <kdrudy@kyru.net>
2018-05-28 22:24:46 -0500

    Updated code and display

--- src/main/java/com/kyrutech/kylefyle/Main.java
+++ src/main/java/com/kyrutech/kylefyle/Main.java
@@ -3,24 +3,38 @@ package com.kyrutech.kylefyle;
 import spark.ModelAndView;
 import spark.template.mustache.MustacheTemplateEngine;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.File;
+import java.util.*;
 
 import static spark.Spark.*;;
 
 public class Main {
     public static void main(String[] args) {
-        if(args.length < 1) {
-            get("/", (req, res) -> {
-                Map<String, Object> model = new HashMap<>();
-                model.put("error", "No file argument to display");
-                return new MustacheTemplateEngine().render(
-                        new ModelAndView(model, "/templates/index.html")
-                );
-            });
-        } else {
-
-        }
-        get("/hello", (req, res) -> "Hello World");
+        port(8080);
+        get("/", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            if (args.length < 1) { //No file passed in
+                model.put("hasError", true);
+                model.put("error", "No file argument to display.");
+            } else {
+                File f = new File(args[0]);
+                if (!f.exists() || f.isDirectory()) { //File doesn't exist or is a directory
+                    model.put("hasError", true);
+                    model.put("error", args[0] + " doesn't exist or is not a file.");
+                } else { //Should be good
+                    Scanner sc = new Scanner(f);
+                    List<String> content = new ArrayList<>();
+                    while(sc.hasNextLine()) {
+                        content.add(sc.nextLine());
+                    }
+                    model.put("hasError", false);
+                    model.put("fileName", f.getName());
+                    model.put("content", content);
+                }
+            }
+            return new MustacheTemplateEngine().render(
+                    new ModelAndView(model, "index.html")
+            );
+        });
     }
 }
